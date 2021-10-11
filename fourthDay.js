5d2f21b68eda84b7175f161fa4a6d16048f663af
Chethan <chethanmurthy7518@gmail.com>
2021-10-11 16:44:47 +0530

    Commiting

---
+++ fourthDay.js
@@ -0,0 +1,23 @@
+let person =[{
+    firtName:"salamn",
+    lastName:"khan"
+},
+{
+    firtName:"sothing",
+    lastName:"khan"
+},
+{
+    firtName:"boom",
+    lastName:"khan"
+},
+{
+    firtName:"nothing",
+    lastName:"khan"
+}
+
+let fullName = person.map((Name)=>{
+    return [Name.firtName, Name.lastName].join(' ')
+});
+
+
+]
